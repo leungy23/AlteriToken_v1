130e0f3c1420be581072d8a27029df3f0fb22361
Joe Leung <88jcwl@gmail.com>
2022-12-31 18:14:42 +0000

    updated smartcontract (requires fix)

--- contracts/AlteriToken.sol
+++ contracts/AlteriToken.sol
@@ -1,22 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// TODO work out how to ensure recipients, vendors and verifiers cannot be in eachothers list - some require function...  tbc. require(user =! recipientList[]);
+// TODO work out how to ensure person(s), vendors and verifiers cannot be in eachothers list - some require function...  tbc. require(person =! vendorList[]);
 // TODO create the TOKEN and ensure this contract only uses this specific token - quite tired so not sure if this is essential or completely missing the point.
 
 contract AlteriToken {
+    struct person {
+        string firstName;
+        string lastName;
+        address personAddress;
+    }
+
+    struct verifier {
+        string verifierName;
+        address verifierAddress;
+    }
+    verifier[] public verifiers;
+
+    struct vendor {
+        string vendorName;
+        address vendorAddress;
+    }
+
     // Mapping from user addresses to their balances
     mapping(address => uint256) public balances;
 
     // Mapping from user addresses to their roles
     mapping(address => bool) public verifierList;
-    mapping(address => bool) public recipientList;
+    mapping(address => bool) public personList;
     mapping(address => bool) public vendorList;
 
     // Address of the contract owner
     address public owner;
-
     // Address to which vendors can send tokens
+
     address public treasurerAddress;
 
     // Constructor to set the contract owner
@@ -24,13 +41,17 @@ contract AlteriToken {
         owner = msg.sender;
     }
 
-    // Function to add a user to the verifierList
-    function addVerifier(address user) public {
+    // Function to add a verifier to the verifierList
+    function addVerifier(string memory _verifier, address _vAddress) public {
         // Only the contract owner can add users to the verifierList
         require(msg.sender == owner, "Only the owner can add verifiers");
 
         // Add the user to the verifierList
-        verifierList[user] = true;
+        verifier memory Verifier = verifier({
+            verifierName: _verifier,
+            verifierAddress: _vAddress
+        });
+        verifiers.push(verifier);
     }
 
     // Function to remove a user from the verifierList
@@ -42,34 +63,28 @@ contract AlteriToken {
         verifierList[user] = false;
     }
 
-    // Function to add a user to the recipientList
-    function addRecipient(address user) public {
-        // Only users in the verifierList can add users to the recipientList
-        require(verifierList[msg.sender], "Only verifiers can add recipients");
+    // Function to add a user to the personList
+    function addPerson(address user) public {
+        // Only users in the verifierList can add users to the personList
+        require(verifierList[msg.sender], "Only verifiers can add people");
 
-        // Add the user to the recipientList
-        recipientList[user] = true;
+        // Add the user to the personList
+        personList[user] = true;
     }
 
-    // Function to remove a user from the recipientList
-    function removeRecipient(address user) public {
-        // Only users in the verifierList can remove users from the recipientList
-        require(
-            verifierList[msg.sender],
-            "Only verifiers can remove recipients"
-        );
+    // Function to remove a user from the personList
+    function removePerson(address user) public {
+        // Only users in the verifierList can remove users from the personList
+        require(verifierList[msg.sender], "Only verifiers can remove people");
 
-        // Remove the user from the recipientList
-        recipientList[user] = false;
+        // Remove the user from the personList
+        personList[user] = false;
     }
 
     // Function to transfer tokens to a user in the vendorList
-    function transfer(address recipient, uint256 amount) public {
+    function spend(address recipient, uint256 amount) public payable {
         // Only users in the recipientList can transfer tokens to users in the vendorList
-        require(
-            recipientList[msg.sender],
-            "Only recipients can transfer tokens"
-        );
+        require(personList[msg.sender], "Only recipients can transfer tokens");
 
         // Ensure the recipient is in the vendorList
         require(vendorList[recipient], "Recipient must be in the vendorList");
